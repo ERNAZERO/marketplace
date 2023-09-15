e84b722ebe26f3a26e5c903cba71cf43dae755ce
Ernaz Erkinbekov <ernazerkinbekov@MacBook-Air-Ernaz.local>
2023-09-15 16:22:51 +0600

    Добавляю доеер

--- marketplaceProject/settings.py
+++ marketplaceProject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_$q0fx!&^es!f0j=w@#*^%z*=_^6c%^0enc+=!tetb%870@@d-'
-
+# SECRET_KEY = ''
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -44,8 +45,8 @@ INSTALLED_APPS = [
 
 
     #apps
-    'product',
-    'user',
+    'apps.product',
+    'apps.user',
 
 
 ]
@@ -102,16 +103,28 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#   'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'asia_shop',
+#         'USER': 'ernaz',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#          }
+# }
+
 DATABASES = {
-  'default': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'asia_shop',
-        'USER': 'ernaz',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
-         }
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT'),
+    }
 }
+
 
 
 # Password validation
@@ -148,7 +161,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = (BASE_DIR / 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
